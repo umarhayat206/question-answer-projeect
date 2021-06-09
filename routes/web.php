4bab0737a017e74a6237cab5a38beafc90d1fce1
umarhayat206 <56934498+umarhayat206@users.noreply.github.com>
2021-06-09 06:22:36 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,30 @@
 <?php
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/question','QuestionController@index')->name('questionhome');
+
+Route::get('/create','QuestionController@create')->name('question.create');
+
+Route::POST('/store','QuestionController@store')->name('question.store');
+Route::get('question/{id}/edit','QuestionController@edit')->name('question.edit');
+Route::Post('question/{id}/update','QuestionController@update')->name('question.update');
+Route::get('question/{id}/delete','QuestionController@destroy')->name('question.delete');
+Route::get('/question/{slug}','QuestionController@show')->name('question.show');
+
+
+// Routes for answers
+
+Route::Post('question/{question}/answer/store','AnswerController@store')->name('question.answer.store');
+Route::get('question/{question}/answer/{answer}/edit','AnswerController@edit')->name('question.answer.edit');
+Route::Post('question/{question}/answer/{answer}/update','AnswerController@update')->name('question.answer.update');
+Route::get('question/{question}/answer/{answer}/delete','AnswerController@destroy')->name('question.answer.delete');
 
 /*
 |--------------------------------------------------------------------------
@@ -526,29 +552,3 @@ https://www.bootdey.com/snippets/view/profile-with-data-and-skills
 
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-
-Route::get('/question','QuestionController@index')->name('questionhome');
-
-Route::get('/create','QuestionController@create')->name('question.create');
-
-Route::POST('/store','QuestionController@store')->name('question.store');
-Route::get('question/{id}/edit','QuestionController@edit')->name('question.edit');
-Route::Post('question/{id}/update','QuestionController@update')->name('question.update');
-Route::get('question/{id}/delete','QuestionController@destroy')->name('question.delete');
-Route::get('/question/{slug}','QuestionController@show')->name('question.show');
-
-
-// Routes for answers
-
-Route::Post('question/{question}/answer/store','AnswerController@store')->name('question.answer.store');
-Route::get('question/{question}/answer/{answer}/edit','AnswerController@edit')->name('question.answer.edit');
-Route::Post('question/{question}/answer/{answer}/update','AnswerController@update')->name('question.answer.update');
-Route::get('question/{question}/answer/{answer}/delete','AnswerController@destroy')->name('question.answer.delete');
